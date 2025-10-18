4aaaef4b71f162b0ad7a40a77be363ccdec4ba21
clln1 <yuriferreira245@gmail.com>
2025-10-18 13:42:04 -0300

    chore: import config from .env and add a brief explanatory comment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
+
+config({ path: '.env' });
 
 export default defineConfig({
     out: './drizzle',
     schema: './src/db/schema/**/*.ts',
     dialect: 'postgresql',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL!, // Altere aqui para process.env.CLOUD_DATABASE_URL! depois de setar a url do banco remoto (se for usar o remoto)
     },
     
     breakpoints: true,
