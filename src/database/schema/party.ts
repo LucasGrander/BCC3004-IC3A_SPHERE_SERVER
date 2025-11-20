2f38da302d7548e7c52930f2093682e2b98b7673
clln1 <yuriferreira245@gmail.com>
2025-11-19 22:33:21 -0300

    chore(db): unite Address table columns to Party table, removed its relations, generate and apply migrations to remote DB

--- src/database/schema/party.ts
+++ src/database/schema/party.ts
@@ -4,19 +4,24 @@ import { person } from "./person";
 import { relations } from "drizzle-orm";
 import { pgEnum } from "drizzle-orm/pg-core";
 import { partyService } from "./partyService";
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from "drizzle-zod";
+import z from "zod";
+import { db } from "..";
 
+export const partyTypes = [
 
-export const partyType= pgEnum('party_type', 
-    [
-        'PoolParty', 
-        'Eletrônica',
-        'Aniversário',
-        'Chá Revelação',
-        'Confraternização',
-        'Formatura',
-        'Casamento',
+    "PoolParty",
+    "Eletrônica",
+    "Aniversário",
+    "Chá Revelação",
+    "Confraternização",
+    "Formatura",
+    "Casamento",
         
-    ])
+] as const;
+
+
+export const partyType = pgEnum('party_type', partyTypes);
 
 
 export const party = pgTable("party", {
@@ -24,16 +29,19 @@ export const party = pgTable("party", {
     id: serial().primaryKey().notNull(),
     name: text('name').notNull(),
     date: date().notNull(),
+    street: text('address_street').notNull(),
+    number: text('address_number').notNull(),
+    complement: text('address_complement'),
+    neighborhood: text('address_neighborhood').notNull(),
+    city: text('address_city').notNull(),
     type: partyType('party_type').notNull(),
-    person_id: integer('person_id').references(() => person.id, {onDelete: 'cascade'}).notNull(),
-    address_id: integer('address_id').references(() => address.id, {onDelete: 'cascade'}).notNull()
+    person_id: integer('person_id').references(() => person.id, { onDelete: 'cascade' }).notNull(),
     
 });
 
 export const partyRelations = relations(party, ({ one, many }) => ({
 
-    address: one(address, {fields: [party.address_id], references: [address.id]}),
-    person: one(person,{fields: [party.person_id], references: [person.id]} ),
+    person: one(person, { fields: [party.person_id], references: [person.id] }),
     partyService: many(partyService)
 
 }));
