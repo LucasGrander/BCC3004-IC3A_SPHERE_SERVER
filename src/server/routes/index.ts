18c25435a3e7819612b7167459cfc2736ebad44c
Nicolas-331 <nicolasroliveira159@gmail.com>
2025-12-02 11:43:53 -0300

    feat: adiciona middleware de autenticação nas rotas privadas existentes
    Os testes dos endpoits de Party vão parar de founcionar pois não contém nas requisições (teste) o header Authorization, uma nova issue será adicionada para a correção dos mesmos.

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -11,11 +11,11 @@ router.get('/', (_, res) =>{
     return res.status(StatusCodes.IM_A_TEAPOT).send('Olá Yuri');
 });
 
-router.post('/party', PartyController.createValidator, PartyController.create);
-router.put('/party/:id', PartyController.updateByIdValidator, PartyController.updateById);
-router.get('/party/all/:id', PartyController.getAllValidator, PartyController.getAll);
-router.get('/party/:id', PartyController.getByIdValidator, PartyController.getById);
-router.delete('/party/:id', PartyController.deleteByIdValidator, PartyController.deleteById);
+router.post('/party', ensureAuthenticated, PartyController.createValidator, PartyController.create);
+router.put('/party/:id', ensureAuthenticated, PartyController.updateByIdValidator, PartyController.updateById);
+router.get('/party/all/:id', ensureAuthenticated, PartyController.getAllValidator, PartyController.getAll);
+router.get('/party/:id', ensureAuthenticated, PartyController.getByIdValidator, PartyController.getById);
+router.delete('/party/:id', ensureAuthenticated, PartyController.deleteByIdValidator, PartyController.deleteById);
 
 router.post('/signup', PersonController.signUpValidator, PersonController.signUp);
 router.post('/signin', PersonController.signInValidator, PersonController.signIn);
